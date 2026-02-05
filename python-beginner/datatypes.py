93f01291dd5015567ecb3b6133d195be3ad011b3
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-05 18:43:58 +0530

    Add Python data types example

---
+++ python-beginner/datatypes.py
@@ -0,0 +1,29 @@
+# Python Data Types Example
+
+# Integer
+age = 21
+print("Age:", age)
+
+# Float
+height = 5.6
+print("Height:", height)
+
+# String
+name = "Ankita"
+print("Name:", name)
+
+# Boolean
+is_student = True
+print("Is Student:", is_student)
+
+# List
+subjects = ["Python", "AI", "ML"]
+print("Subjects:", subjects)
+
+# Dictionary
+student = {
+    "name": "Ankita",
+    "age": 21,
+    "course": "Computer Science"
+}
+print("Student Details:", student)
