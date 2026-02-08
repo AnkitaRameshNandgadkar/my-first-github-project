7e79bfe5bc92bccfa07335071ce0ff97ef547f99
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-08 20:25:22 +0530

    add calculator using function

---
+++ python-beginner/calculator.py
@@ -0,0 +1,13 @@
+# Simple Calculator using Functions
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Addition:", add(x, y))
+print("Subtraction:", subtract(x, y))
