a478bf473d9f2fd3dc5e4cbd2106128940b127be
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-10 21:48:02 +0530

    Add pass and fail logic using list and loop

---
+++ python-beginner/pass_fail_list.py
@@ -0,0 +1,9 @@
+# Check pass/fail status for students
+
+marks = [78, 35, 92, 40, 67]
+
+for mark in marks:
+    if mark >= 40:
+        print(mark, "- Pass")
+    else:
+        print(mark, "- Fail")
