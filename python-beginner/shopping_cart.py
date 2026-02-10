29b44a568e4e8963b17b110739a716f429e6d34e
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-10 21:50:08 +0530

    Add shopping cart total calculation using list

---
+++ python-beginner/shopping_cart.py
@@ -0,0 +1,11 @@
+# Simple shopping cart example
+
+prices = [120, 250, 90, 60]
+
+total_bill = 0
+
+for price in prices:
+    total_bill += price
+
+print("Items prices:", prices)
+print("Total Bill Amount:", total_bill)
