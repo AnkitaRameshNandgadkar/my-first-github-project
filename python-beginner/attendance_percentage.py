d2e612b497aa5e32ad6f952a5d0c94d11d6e4fbd
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-10 21:55:18 +0530

    Add attendance percentage calculation using list

---
+++ python-beginner/attendance_percentage.py
@@ -0,0 +1,8 @@
+# Attendance percentage calculation
+
+total_classes = 100
+attended_classes = [85, 90, 76, 88]
+
+for attended in attended_classes:
+    percentage = (attended / total_classes) * 100
+    print("Attendance:", percentage, "%")
