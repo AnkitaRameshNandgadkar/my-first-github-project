aa988827c8d975e9819158d0acf7246c05032c1f
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-13 20:41:45 +0530

    Add utility bill calculation using dictionary

---
+++ python-beginner/utility_bill_dictionary.py
@@ -0,0 +1,12 @@
+# Utility bill calculation using dictionary
+
+student = {
+    "name": "Ankita",
+    "electricity_units": 120,
+    "rate_per_unit": 5
+}
+
+bill = student["electricity_units"] * student["rate_per_unit"]
+
+print("Student:", student["name"])
+print("Electricity Bill:", bill)
