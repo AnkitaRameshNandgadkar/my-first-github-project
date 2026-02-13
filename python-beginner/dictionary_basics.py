765a97335204115646630da78cd665c59c1b89ee
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-02-13 20:40:13 +0530

    Add basic dictionary example with student information

---
+++ python-beginner/dictionary_basics.py
@@ -0,0 +1,12 @@
+# Basic dictionary example
+
+student = {
+    "name": "Ankita",
+    "age": 21,
+    "course": "Computer Science"
+}
+
+print("Student Information:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Course:", student["course"])
