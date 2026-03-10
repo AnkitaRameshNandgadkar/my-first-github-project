174d72a6db9196278c618d3c58069cd0176448ae
AnkitaRameshNandgadkar <ankitanandgadkar@gmail.com>
2026-03-10 15:04:06 +0530

    Add palindrome check using two pointer technique

---
+++ python-beginner/placement/palindrome_two_pointer.py
@@ -0,0 +1,21 @@
+# Check palindrome using two pointers
+
+text = "madam"
+
+left = 0
+right = len(text) - 1
+
+is_palindrome = True
+
+while left < right:
+    if text[left] != text[right]:
+        is_palindrome = False
+        break
+    left += 1
+    right -= 1
+
+
+if is_palindrome:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
